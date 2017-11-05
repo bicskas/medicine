000e3667e8fc50e77aca1f7627a27c60a613265f
BicskS <bicskas94@gmail.com>
2017-11-05 21:08:22 +0100

    átalakítás és rekurzív rész kialakítás (nem tökéletes)

--- finder.py
+++ finder.py
@@ -4,15 +4,49 @@ from random import randint
 import requests
 from bs4 import BeautifulSoup
 import network
+import urllib.request
+from urllib.parse import urljoin
 import env
+import re
+
+
+
+global visited
+visited = []
+
+global queue
+queue = []
+
+
+def setBaseUrl(url):
+    global baseurl
+    baseurl = url
+
+
+def addToVisited(url):
+    visited.append(url)
+
+
+def addToQueue(url):
+    if not url in queue and not url in visited:
+        queue.append(url)
+
+
+def removeFromQueue(url):
+    queue.remove(url)
 
 
 # az adott oldal html részét adja vissza
 def getParse(baseurl):
     url = baseurl
-    response = requests.get(url)
+    addToVisited(url)
+    html_page = urllib.request.urlopen(url)
+    try:
+        pass
+    except ValueError:
+        print()
     # parse html
-    parse = str(BeautifulSoup(response.content, "lxml"))
+    parse = BeautifulSoup(html_page, 'lxml')
     return parse
 
 
@@ -23,13 +57,21 @@ def getURL(page):
     :param page: html of web page (here: Python home page)
     :return: urls in that page
     """
-    start_link = page.find("a href")
-    if start_link == -1:
-        return None, 0
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1: end_quote]
-    return url, end_quote
+    t_links = []
+    links = []
+
+    # for link in soup.findAll('a', attrs={'href': re.compile("^htt")}):
+    for link in page.findAll('a'):
+        if not link.get('href') is None and not link.get('href').startswith('#') and link.get(
+                'href') != baseurl:
+            t_links.append(urljoin(baseurl, link.get('href')))
+
+    for l in t_links:
+        if l.startswith('http') and not l in visited:
+            links.append(l)
+            addToQueue(l)
+
+    return links
 
 
 # a getUrl alapján készít egy tömböt a linkekből
@@ -46,15 +88,27 @@ def getSubLink(page):
 
 
 def kiir(sublinks):
+    # print(baseurl)
     for i in range(len(sublinks)):
-        print(sublinks[i])
+        # print(sublinks[i])
         network.elment(sublinks[i])
         network.addToBase(baseurl, sublinks[i])
 
 
-baseurl = "http://ferling.hu"
-visited = []
+starturl = "http://pte.hu"
+setBaseUrl(starturl)
 page = getParse(baseurl)
+getURL(page)
 network.deleteAll()
 network.elment(baseurl)
-kiir(getSubLink(page))
+kiir(getURL(page))
+print('ELSŐ RÉSZ')
+
+for q in queue:
+    if not q in visited:
+        print(q)
+        setBaseUrl(q)
+        removeFromQueue(q)
+        page = getParse(baseurl)
+        getURL(page)
+        kiir(getURL(page))
