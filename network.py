69d1c92b10e14c6539cb79838491e3a9700608c0
BicskS <bicskas94@gmail.com>
2018-01-28 20:56:13 +0100

    további ábrázolások, inkább csak tesztkódok, alakítani kell még rajta

--- network.py
+++ network.py
@@ -1,4 +1,6 @@
 import database
+import collections
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -22,23 +24,50 @@ for e in edges:
 # print(G.number_of_edges())
 
 
+#------------------------------------eloszlás kirajzolása a ábrával együtt------------------------------------
+# degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+# # print "Degree sequence", degree_sequence
+# dmax = max(degree_sequence)
+# print(degree_sequence)
+# plt.loglog(degree_sequence, 'b-', marker='H',markeredgecolor='g')
+# plt.title("Degree rank plot")
+# plt.ylabel("degree")
+# plt.xlabel("rank")
+# # plt.xscale('linear')
+# # plt.yscale('linear')
+#
+# H = nx.Graph(G)
+# # draw graph in inset
+# plt.axes([0.45, 0.45, 0.45, 0.45])
+# Hcc = sorted(nx.connected_component_subgraphs(H), key=len, reverse=True)[0]
+# pos = nx.spring_layout(Hcc)
+# plt.axis('off')
+# nx.draw_networkx_nodes(Hcc, pos, node_size=20)
+# nx.draw_networkx_edges(Hcc, pos, alpha=0.4)
+# plt.show()
+# # plt.savefig("test.png",dpi=2000)
 
-# eloszlás kirajzolása a ábrával együtt
-degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+#---------------------------histogram------------------------------------------------
+degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
 # print "Degree sequence", degree_sequence
-dmax = max(degree_sequence)
+degreeCount = collections.Counter(degree_sequence)
+deg, cnt = zip(*degreeCount.items())
 
-plt.loglog(degree_sequence, 'b-', marker='H',markeredgecolor='g')
-plt.title("Degree rank plot")
-plt.ylabel("degree")
-plt.xlabel("rank")
+fig, ax = plt.subplots()
+plt.bar(deg, cnt, width=0.80, color='b')
+
+plt.title("Degree Histogram")
+plt.ylabel("Count")
+plt.xlabel("Degree")
+ax.set_xticks([d + 0.4 for d in deg])
+ax.set_xticklabels(deg)
 
 # draw graph in inset
-plt.axes([0.45, 0.45, 0.45, 0.45])
+plt.axes([0.4, 0.4, 0.5, 0.5])
 Gcc = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)[0]
-pos = nx.spring_layout(Gcc)
+pos = nx.spring_layout(G)
 plt.axis('off')
-nx.draw_networkx_nodes(Gcc, pos, node_size=20)
-nx.draw_networkx_edges(Gcc, pos, alpha=0.4)
+nx.draw_networkx_nodes(G, pos, node_size=20)
+nx.draw_networkx_edges(G, pos, alpha=0.4)
 
 plt.show()
